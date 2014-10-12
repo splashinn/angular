53a60e5c0340f89eaa4e646102d61b3b99299183
Kyle Maune <kmaune71@gmail.com>
2014-10-12 12:50:17 -0700

    added JS files

---
+++ js/app.js
@@ -0,0 +1,13 @@
+angular.module('EmailApp', [
+  'ngRoute'
+]).config(function ( $routeProvider ) {
+  'use strict';
+  $routeProvider
+    .when('/inbox', {
+      templateUrl: 'views/inbox.html',
+      controller: 'InboxCtrl'
+    })
+    .otherwise({
+      redirectTo: '/inbox'
+    });
+});
